5b54eeebb47720bc1a14fa116782c705819f12a0
Tsunamins <reillyamr@gmail.com>
2019-12-12 18:45:21 -0500

    establish connection to api

--- note-frontend/src/adapters/NotesAdapter.js
+++ note-frontend/src/adapters/NotesAdapter.js
@@ -1,11 +1,12 @@
 class NotesAdapter { //will communicate with backend API, sometimes called service
     constructor() {
-        this.baseUrl = "http://localhost:3000/api/v1/notes" 
+        this.baseUrl = 'http://localhost:3000/api/v1/notes'
 
     }
 
     getNotes() {//consider instance method
-        return fetch(this.baseUrl).then(res => res.json()) //parse response into json
+        return fetch(this.baseUrl).then(res => res.json()
+        ) //parse response into json
     }
 }
 
