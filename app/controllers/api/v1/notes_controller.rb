cb65fb805a5cbaf20b5b0383ea772f34a50d83ac
Tsunamins <reillyamr@gmail.com>
2019-12-12 15:40:05 -0500

    add CRUD to notes controller

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -4,4 +4,32 @@ class Api::V1::NotesController < ApplicationController
         @notes = Note.All
         render json: @notes, status: 200 
     end 
+
+    def show
+        @note = Note.find(params[:id])
+        render json: @note, status: 200
+    end 
+
+    def create
+        #same here later change to Note.new, following if statement with applied conventions
+        @note = Note.create(note_params)
+        render json: @note, status: 200
+    end 
+
+    def update
+        #should add back in usual conventions if @note.update (works), then update etc, if not then error or return to previous process, etc refer to previous rails apps
+        @note = Note.find(params[:id])
+        @note.update(note_params)
+        render json: @note, status: 200
+    end 
+    
+    def destroy
+        @note = Note.find(params[:id])
+        @note.delete
+        render json: {noteId: @note.id} #do not want to pass back all info, just the reference to what was deleted
+
+    private
+        def note_params
+            params.require(:note).permit(:body) #for now
+        end 
 end
