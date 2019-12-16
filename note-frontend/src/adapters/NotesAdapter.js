33283068e3c37b9fac4c37679b4d6a29b9c7db2a
Tsunamins <reillyamr@gmail.com>
2019-12-16 14:50:39 -0500

    add header to post request

--- note-frontend/src/adapters/NotesAdapter.js
+++ note-frontend/src/adapters/NotesAdapter.js
@@ -14,10 +14,15 @@ class NotesAdapter { //will communicate with backend API, sometimes called servi
         const note = {
             body: value
         }
+        //debugger
         return fetch(this.baseUrl,{//POST request takes in 2nd arg, with options passed in
             method: 'POST', 
+            headers: {                
+                'content-type': 'application/json'
+            },
             body: JSON.stringify({note: note}) //stringify note object previously made reference, this note: note patter is specific to ES6
         }) 
+        .then(res => res.json()) //send parsed json object back to Notes class in createNote 
     }
 }
 
