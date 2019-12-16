90de5edd27831a34b0ec95bf4edc79095d6b5bd1
Tsunamins <reillyamr@gmail.com>
2019-12-16 13:05:12 -0500

    add points of tutorial

--- note-frontend/src/components/app.js
+++ note-frontend/src/components/app.js
@@ -1,6 +1,8 @@
 class App {
-    constructor(){
+    constructor(){ //new in index.js gets this constructor
         
-        this.notes = new Notes() //create new instance of notes class
+        this.notes = new Notes() //new instance of notes class
+        //app assigns a new property called notes
+        //and invokes notes component in src/components/notes.js
     }
 }
