94b31df06915f15b972a649036933fd814429c4e
Tsunamins <reillyamr@gmail.com>
2019-12-16 13:06:36 -0500

    change to .push from Note class creating Note objs

--- note-frontend/src/components/notes.js
+++ note-frontend/src/components/notes.js
@@ -1,17 +1,19 @@
 class Notes {
     constructor() {
-        this.notes = []
-        this.adapter = new NotesAdapter()
+        this.notes = [] //set property called notes to empty array
+        this.adapter = new NotesAdapter() //set adapter prop to a new instance of notesadapter
        // this.bindEventListeners() //to be created
         this.fetchAndLoadNotes()
     }
 
-    fetchAndLoadNotes(){
+    fetchAndLoadNotes() {
 
         this.adapter
         .getNotes()
         .then(notes => {
-            notes.forEach(note => this.notes.push(note))
+            
+            notes.forEach(note => this.notes.push(new Note(note))) //changes to Note objects to be able to take advantage of the note class
+            console.log(this.notes) //not necessary, for viewing purposes, see Notes as objects in console log
         })
         .then(() => {
             this.render()
