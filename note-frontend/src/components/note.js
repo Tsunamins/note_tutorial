c2fab97cbb2b44a3976d1a17d095aa71b627ef12
Tsunamins <reillyamr@gmail.com>
2019-12-16 13:05:27 -0500

    create Note class

---
+++ note-frontend/src/components/note.js
@@ -0,0 +1,8 @@
+class Note {
+    constructor(noteJSON){
+        this.id = noteJSON.id
+        this.body = noteJSON.body
+    }
+}
+
+//use this constructor to create a new note? as iterating over note array
