6d7c7383b4f53cad0d0cc274c7b88f8ece743620
Tsunamins <reillyamr@gmail.com>
2019-12-16 14:18:44 -0500

    add binds/eventList and createNote func

--- note-frontend/src/components/notes.js
+++ note-frontend/src/components/notes.js
@@ -2,8 +2,24 @@ class Notes {
     constructor() {
         this.notes = [] //set property called notes to empty array
         this.adapter = new NotesAdapter() //set adapter prop to a new instance of notesadapter
-       // this.bindEventListeners() //to be created
+        this.initBindingsAndEventListeners() //to be created
         this.fetchAndLoadNotes()
+    }
+
+    initBindingsAndEventListeners(){
+        this.notesContainer = document.getElementById('notes-container')
+        this.newNoteBody = document.getElementById('new-note-body')
+        this.noteForm = document.getElementById('new-note-form')
+        this.noteForm.addEventListener('submit', this.createNote.bind(this)) //params expl:(when form *submit*ted, use this *function*), func params bind *this* to Notes class, considered hard bind, otherwise will bind the form and not the notes class
+    }
+
+    createNote(e){
+        
+        e.preventDefault()
+        //to see if function is working: console.log('note is being created')
+        const value = this.newNoteBody.value
+
+        this.adapter.createNote(value)
     }
 
     fetchAndLoadNotes() {
@@ -25,9 +41,9 @@ class Notes {
         
         
 
-        const notesContainer = document.getElementById('notes-container')
-        notesContainer.innerHTML = this.notes.map(note => `<li>${note.body}</li>`).join('') //join because other wise each <li> marker is in individual array which won't process in html doc, join the array to make a string to render in in html document
-        
+        //moved to new function above so as not to do it with every render: const notesContainer = document.getElementById('notes-container')
+        //part of this moved to note.js to renderLi object: this.notesContainer.innerHTML = this.notes.map(note => `<li>${note.body}</li>`).join('') //join because other wise each <li> marker is in individual array which won't process in html doc, join the array to make a string to render in in html document
+        this.notesContainer.innerHTML = this.notes.map(note => note.renderLi()).join('')
         
 
 
