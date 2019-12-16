69d2bb8003235c7e2cb8ed74e57c3e1c9c2552b4
Tsunamins <reillyamr@gmail.com>
2019-12-16 14:18:00 -0500

    simplify dom manip making li its own function

--- note-frontend/src/components/note.js
+++ note-frontend/src/components/note.js
@@ -3,6 +3,10 @@ class Note {
         this.id = noteJSON.id
         this.body = noteJSON.body
     }
+
+    renderLi(){
+        return `<li>${this.body}</li>` //new place for dom manip
+    }
 }
 
 //use this constructor to create a new note? as iterating over note array
